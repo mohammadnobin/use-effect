ba67dc80250c54b8fa073dfd654860322d220b9d
MdNobinfd <mdnobin.fdr@gmail.com>
2024-07-04 13:26:33 +0600

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,109 @@
+import React, { useEffect, useRef, useState } from "react";
+import Container from "./Container";
+import { HiOutlineBars3BottomLeft } from "react-icons/hi2";
+import { FaSearch, FaUser, FaShoppingCart } from "react-icons/fa";
+import { MdOutlineArrowDropDown } from "react-icons/md";
+import { RiCloseLargeFill } from "react-icons/ri";
+import cartimg from '../assets/cartimg.png'
+
+const Header = () => {
+  let [catagorishow ,setcatagorishow] = useState(false)
+  let catagoriRef = useRef()
+  useEffect(()=>{
+    window.addEventListener("click",(e)=>{
+      if (catagoriRef.current.contains(e.target) == true) {
+        setcatagorishow(!catagorishow)
+      } else{
+        setcatagorishow(false)
+      }
+    })
+  },[catagorishow])
+
+
+  return (
+    <section id="header" className="bg-[#F5F5F3] py-[25px]">
+      <Container>
+        <div className="flex items-center gap-4">
+          <div className="w-1/4 relative">
+            <div ref={catagoriRef} className="flex items-center">
+              <div className="">
+                <HiOutlineBars3BottomLeft className="text-[28px] cursor-pointer" />
+              </div>
+              <div className="">
+                <h4 className="text-[#262626] font-dm-sans font-normal text-base leading-[18px] hidden lg:block ">Shop by Category</h4>
+              </div>
+            </div>
+            {catagorishow &&
+                  <div className="absolute  top-10 lg:top-7  left-0 w-[300px] z-10 bg-[#262626] ">
+                  <ul>
+                    <li className="font-dm-sans text-[#b1adad] font-normal text-base hover:pl-[31px] pl-[21px] py-[16px] duration-500 cursor-pointer hover:text-white ease-in-out border-b-2 border-[#6b6969]">Accesories</li>
+                    <li className="font-dm-sans text-[#b1adad] font-normal text-base hover:pl-[31px] pl-[21px] py-[16px] duration-500 cursor-pointer hover:text-white ease-in-out border-b-2 border-[#6b6969]">Furniture</li>
+                    <li className="font-dm-sans text-[#b1adad] font-normal text-base hover:pl-[31px] pl-[21px] py-[16px] duration-500 cursor-pointer hover:text-white ease-in-out border-b-2 border-[#6b6969]">Electronics</li>
+                    <li className="font-dm-sans text-[#b1adad] font-normal text-base hover:pl-[31px] pl-[21px] py-[16px] duration-500 cursor-pointer hover:text-white ease-in-out border-b-2 border-[#6b6969]">Clothes</li>
+                    <li className="font-dm-sans text-[#b1adad] font-normal text-base hover:pl-[31px] pl-[21px] py-[16px] duration-500 cursor-pointer hover:text-white ease-in-out border-b-2 border-[#6b6969]">Bags</li>
+                    <li className="font-dm-sans text-[#b1adad] font-normal text-base hover:pl-[31px] pl-[21px] py-[16px] duration-500 cursor-pointer hover:text-white ease-in-out border-b-2 border-[#6b6969]"></li>
+                  </ul>
+                </div>
+             }
+          </div>
+          <div className="w-2/4">
+            <div className="relative">
+              <input className="w-full h-[50px] placeholder:text-[#C4C4C4] text-black pl-3" type="text" placeholder="Search Products" />
+              <div className="absolute top-[50%] translate-y-[-50%] right-5 ">
+                 <FaSearch className="text-xl cursor-pointer" /> 
+              </div>
+            </div>
+          </div>
+          <div className="w-1/4">
+            <div className="relative flex justify-end gap-x-2  lg:gap-5"> 
+              <div className="flex relative">
+                <FaUser className="text-xl cursor-pointer"/>
+                <MdOutlineArrowDropDown className="text-xl cursor-pointer" />
+                <div className="absolute top-9  right-0 text-center w-[200px]">
+                  <div className="">
+                    <button className="w-[196px] py-[16px] bg-white hover:bg-black duration-500 hover:text-white text-black font-dm-sans font-bold text-base mb-1 ">My Account</button>
+                  </div>
+                  <div className="">
+                  <button className="w-[196px] py-[16px] bg-white hover:bg-black duration-500 hover:text-white text-black font-dm-sans font-bold text-base ">Log Out</button>
+                  </div>
+                </div>
+              </div>
+              <div className="">
+                <FaShoppingCart className="text-xl cursor-pointer" />
+              </div>
+              <div className="absolute hidden top-7 right-0 w-[360px]  bg-white">
+                <div className="flex items-center justify-around py-[20px] bg-[#F5F5F3]">
+                  <div className="w-[80px]">
+                    <img src={cartimg} alt="" />
+                  </div>
+                  <div className="">
+                    <h4 className="font-bold font-dm-sans text-base leading-[18px] py-2 ">Black Smart Watch</h4>
+                    <p className="font-bold font-dm-sans text-base leading-[18px] ">$44.00</p>
+                  </div>
+                  <div className="">
+                    <RiCloseLargeFill />
+                  </div>
+                </div>
+                <div className=" p-[20px]">
+                  <div className="pb-[13px]">
+                    <h4 className=" font-dm-sans pb-[13px] text-[18px] leading-[18px]">Subtotal:<span className="font-bold" >$44.00</span></h4>
+                  </div>
+                  <div className="flex justify-around">
+                    <div className="">
+                      <button className="py-[16px] px-[41px] border-black border-2 hover:bg-black hover:text-white duration-500 ">View Card</button>
+                    </div>
+                    <div className="">
+                      <button className="py-[16px] px-[43px] border-black border-2 hover:bg-black hover:text-white duration-500 ">Checkout</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Container>
+    </section>
+  );
+};
+
+export default Header;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react'
+import Container from './Container'
+import { FaBars } from "react-icons/fa6";
+import { RiCloseLargeFill } from "react-icons/ri";
+import navlogo from '../assets/navlogo.png'
+
+
+const Navbar = () => {
+    let [show, setshow] = useState(true)
+  return (
+    <section id='navber' className='bg-white z-[999] shadow-xl'>
+    <Container>
+        <div className="flex py-[32px] lg:justify-normal items-center justify-between  flex-wrap">
+            <div className="w-1/4">
+                <img className='w-[65.01px]' src={navlogo} alt="" />
+            </div>
+            <div className="w-2/4">
+                <ul className={`absolute lg:flex justify-center text-center gap-x-[40px] duration-300 text-white lg:text-[#767676] left-0 lg:bg-transparent  lg:static ${show == true ? "top-[-220px] w-full bg-black z-10" : "top-20 left-0 z-10 w-full bg-black"}`}>
+                   <li className='py-2 lg:py-0 font-dm-sans hover:font-bold hover:text-[#262626] text-base duration-200 hover:cursor-pointer'>Home</li>
+                   <li className='py-2 lg:py-0 font-dm-sans hover:font-bold hover:text-[#262626] text-base duration-200 hover:cursor-pointer'>Shop</li>
+                   <li className='py-2 lg:py-0 font-dm-sans hover:font-bold hover:text-[#262626] text-base duration-200 hover:cursor-pointer'>About</li>
+                   <li className='py-2 lg:py-0 font-dm-sans hover:font-bold hover:text-[#262626] text-base duration-200 hover:cursor-pointer'>Contact</li>
+                   <li className='py-2 lg:py-0 font-dm-sans hover:font-bold hover:text-[#262626] text-base duration-200 hover:cursor-pointer'>Journal</li>
+                </ul>
+            </div>
+            <div onClick={()=>setshow(!show)} className="lg:hidden cursor-pointer">
+                {show == true ? <FaBars className='text-[26px]' /> : <RiCloseLargeFill className='text-[26px]' />}
+           
+            </div>
+        </div>
+    </Container>
+    </section>
+  )
+}
+
+export default Navbar
